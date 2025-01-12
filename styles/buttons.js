a8dbeece8a4567fbbe9eac68ad27d9acf8d305ef
Arsalan Khan (i24k3) <akha3743@gmail.com>
2025-01-12 09:56:36 +0000

    add new feature. default buttons

---
+++ styles/buttons.js
@@ -0,0 +1,12 @@
+import { render } from "./../render.js";
+import { create } from "./../create.js";
+class Buttons 
+{
+  function button1(text, backgroundColor = '', borderColor= '') {
+
+    
+  }
+
+
+}
+
