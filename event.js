72d842ed5920a10018a9a14d061709043e71c701
Arsalan Khan (i24k3) <akha3743@gmail.com>
2025-01-17 19:10:46 +0000

    add docstring + usage

--- event.js
+++ event.js
@@ -1,3 +1,27 @@
+/*
+* @fileoverview Module which provides methods to add events to elements.
+* @author Arsalan Khan (https://github.com/i24k3)
+* @version 1.0.0
+* @license MIT
+* @copyright (c) 2025 Arsalan Khan. All Rights Reserved.
+*/
+
+/**
+* Adds an event listener to a specified element.
+*
+* @param {HTMLElement} element - The DOM element to which the event listener will be added.
+* @param {string} eventType - The type of event to listen for (e.g., 'click', 'mouseover').
+* @param {Function} callback - The function to execute when the event is triggered. (can use arro function)
+*
+* @throws {Warning} Logs a warning if any of the arguments are invalid.
+*
+* Example usage:
+* addEvent(document.getElementById('myButton'), 'click', function() {
+* console.log('Button clicked!');
+* });
+* "--OR--"
+* addEvent(myBtnObj,'click', btnClickCallback);
+*/
 export function addEvent(element, eventType, callback) {
   if (element && typeof eventType === 'string' && typeof callback === 'function') {
     element.addEventListener(eventType, callback);
