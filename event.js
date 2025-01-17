75beafda51acf1e5a5d379621c0a28656c17c3ac
Arsalan Khan (i24k3) <akha3743@gmail.com>
2025-01-17 18:36:30 +0000

    add new method to handle events, fix naming

--- event.js
+++ event.js
@@ -1,8 +1,25 @@
-export function event(element, eventType, callback) {
+export function addEvent(element, eventType, callback) {
   if (element && typeof eventType === 'string' && typeof callback === 'function') {
     element.addEventListener(eventType, callback);
   } else {
-    console.warn('Invalid arguments passed to event().');
+    console.warn('Invalid arguments passed to addEvent().'); }
+}
+
+
+export function setEvent(eventType, callback) {
+  if (this && typeof eventType === 'string' && typeof callback === 'function') {
+    this.addEventListener(eventType, callback);
+  } else {
+    console.warn('Invalid arguments passed to setEvent().');
   }
 }
+
+Object.defineProperty(HTMLElement.prototype, 'setEvent', {
+  value: function(eventType, callback) {
+    setEvent.call(this, eventType, callback);
+  },
+  writable: true,
+  configurable: true
+});
+
 
